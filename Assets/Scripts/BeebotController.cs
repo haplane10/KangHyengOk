26d85bd0a5128963fcf2ff20ba98b426cad626c1
Jungho Park <haplane10>
2021-02-24 01:06:26 +0900

    4일차

--- Assets/Scripts/BeebotController.cs
+++ Assets/Scripts/BeebotController.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Direction
+{
+    None,
+    Forward,
+    Backward,
+    TurnLeft,
+    TurnRight,
+    MAX
+}
+
 public class BeebotController : MonoBehaviour
 {
     public RectTransform beebot;
+    [SerializeField] List<Direction> directions = new List<Direction>();
 
     // Start is called before the first frame update
     void Start()
@@ -21,29 +32,86 @@ public class BeebotController : MonoBehaviour
 
     public void Forward()
     {
-        if ((int)beebot.localEulerAngles.z == 90)
-        {
-            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x - 150, beebot.anchoredPosition.y);
-        }
-
-        if ((int)beebot.localEulerAngles.z == 270)
-        {
-            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x + 150, beebot.anchoredPosition.y);
-        }
-
-        if ((int)beebot.localEulerAngles.z == 0)
-        {
-            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x, beebot.anchoredPosition.y + 150);
-        }
-
-        if ((int)beebot.localEulerAngles.z == 180)
-        {
-            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x, beebot.anchoredPosition.y - 150);
-        }
+        directions.Add(Direction.Forward);
     }
 
     public void Backward()
     {
+        directions.Add(Direction.Backward);
+    }
+
+    public void LeftTurn()
+    {
+        directions.Add(Direction.TurnLeft);
+    }
+
+    public void RightTurn()
+    {
+        directions.Add(Direction.TurnRight);
+    }
+
+    public void PlayBeebot()
+    {
+        StartCoroutine(MoveBeebot());
+    }
+
+    IEnumerator MoveBeebot()
+    {
+        for (int i = 0; i < directions.Count; i++)
+        {
+            switch (directions[i])
+            {
+                case Direction.Forward:
+                    BeebotForward();
+                    break;
+
+                case Direction.Backward:
+                    BeebotBackward();
+                    break;
+
+                case Direction.TurnLeft:
+                    BeebotLeftTurn();
+                    break;
+
+                case Direction.TurnRight:
+                    BeebotRightTurn();
+                    break;
+
+                default:
+                    break;
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+
+        directions.Clear();
+    }
+
+    public void BeebotForward()
+    {
+        if ((int)beebot.localEulerAngles.z == 90)
+        {
+            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x - 150, beebot.anchoredPosition.y);
+        }
+
+        if ((int)beebot.localEulerAngles.z == 270)
+        {
+            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x + 150, beebot.anchoredPosition.y);
+        }
+
+        if ((int)beebot.localEulerAngles.z == 0)
+        {
+            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x, beebot.anchoredPosition.y + 150);
+        }
+
+        if ((int)beebot.localEulerAngles.z == 180)
+        {
+            beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x, beebot.anchoredPosition.y - 150);
+        }
+    }
+
+    public void BeebotBackward()
+    {
         if ((int)beebot.localEulerAngles.z == 90)
         {
             beebot.anchoredPosition = new Vector2(beebot.anchoredPosition.x + 150, beebot.anchoredPosition.y);
@@ -65,12 +133,12 @@ public class BeebotController : MonoBehaviour
         }
     }
 
-    public void LeftTurn()
+    public void BeebotLeftTurn()
     {
         beebot.localEulerAngles = new Vector3(beebot.localEulerAngles.x, beebot.localEulerAngles.y, beebot.localEulerAngles.z + 90);
     }
 
-    public void RightTurn()
+    public void BeebotRightTurn()
     {
         beebot.localEulerAngles = new Vector3(beebot.localEulerAngles.x, beebot.localEulerAngles.y, beebot.localEulerAngles.z - 90);
     }
---
+++ Assets/Scripts/StudyCode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudyCode : MonoBehaviour
+{
+    int A = 10;
+    int B = 20;
+    int C = 30;
+    int Z = 10;
+    int AA ;
+    int AB;
+
+    int[] numbers = new int[200];
+    List<int> numberss = new List<int>();
+    
+    void Awake()
+    {
+        // loop statement
+        for (int i = 0; i < numbers.Length; i = i + 1)
+        {
+            print(numbers[i]);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
